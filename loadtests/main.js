101772513319196024be28413a906b73022e9979
Robin Gustafsson <i@robingustafsson.com>
2017-06-11 23:50:13 +0200

    Fix demo script

--- loadtests/main.js
+++ loadtests/main.js
@@ -1,15 +1,19 @@
 import { check, group, sleep } from "k6";
 import http from "k6/http";
+import { Rate } from "k6/metrics";
+
+export let checkFailureRate = new Rate("check_failure_rate");
 
 export let options = {
     stages: [
         { duration: "5s", target: 10 },
-        { duration: "5s" },
+        { duration: "5s"},
         { duration: "5s", target: 0 }
     ],
     thresholds: {
         http_req_duration: ["p(95)<500"],
-        "http_req_duration{staticAsset:yes}": ["p(95)<200"]
+        "http_req_duration{staticAsset:yes}": ["p(95)<200"],
+        check_failure_rate: ["rate<3"]
     }
 };
 
@@ -18,8 +22,8 @@ export default function() {
         let res = http.get("http://test.loadimpact.com/");
         check(res, {
             "is status 200": (r) => r.status === 200,
-            "is body size correct": (r) => r.body.length === 1176
-        });
+            "is body size 1176 bytes": (r) => r.body.length === 1176
+        }) || checkFailureRate.add(1);
 
         group("Static assets", function() {
             let res = http.batch([
