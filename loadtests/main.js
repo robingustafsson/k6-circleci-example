ebc73bd346038ff389d40e4eae28f9bd34c3b57f
Robin Gustafsson <i@robingustafsson.com>
2017-08-29 18:40:04 +0200

    Demo demo

--- loadtests/main.js
+++ loadtests/main.js
@@ -1,36 +1,40 @@
-import { check, group, sleep } from "k6";
 import http from "k6/http";
+import { check, group, sleep } from "k6";
 import { Rate } from "k6/metrics";
 
-export let checkFailureRate = new Rate("check_failure_rate");
+let checkSuccessRate = new Rate("check_success_rate");
 
 export let options = {
     stages: [
         { duration: "5s", target: 10 },
-        { duration: "5s"},
+        { duration: "5s", target: 10 },
         { duration: "5s", target: 0 }
     ],
     thresholds: {
-        http_req_duration: ["p(95)<100"],
-        "http_req_duration{staticAsset:yes}": ["p(95)<200"],
-        check_failure_rate: ["rate<3"]
+        http_req_duration: ["p(95)<100", "avg<100"],
+        check_success_rate: ["rate>0.9"],
+        "http_req_duration{staticAsset:yes}": ["p(95)<50"],
     }
 };
 
 export default function() {
     group("Front page", function() {
         let res = http.get("http://test.loadimpact.com/");
-        check(res, {
+        let passOrFail = check(res, {
             "is status 200": (r) => r.status === 200,
-            "is body size 1176 bytes": (r) => r.body.length === 1176
-        }) || checkFailureRate.add(1);
+            "is body length 1176 bytes": (r) => r.body.length === 1176
+        });
+        checkSuccessRate.add(passOrFail);
 
         group("Static assets", function() {
             let res = http.batch([
-                ["GET", "http://test.loadimpact.com/style.css", { tags: { staticAsset: "yes" } }],
-                ["GET", "http://test.loadimpact.com/images/logo.png", { tags: { staticAsset: "yes" } }]
+                ["GET", "http://test.loadimpact.com/style.css", { tags: { staticAsset: "yes"} }],
+                ["GET", "http://test.loadimpact.com/images/logo.png", { tags: { staticAsset: "yes"} }]
             ]);
             check(res[0], {
+                "is status 200": (r) => r.status === 200
+            });
+            check(res[1], {
                 "is status 200": (r) => r.status === 200
             });
         });
